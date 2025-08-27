d0c3d47b209ed168182731f6abaf9993f2b7e31f
Tomasz Kasowicz <tomasz.kasowicz@gmail.com>
2025-08-28 00:53:50 +0200

    ci: actions

--- .github/actions/update-npm-packages/test.js
+++ .github/actions/update-npm-packages/test.js
@@ -6,5 +6,5 @@ module.exports = async({github, context}) => {
     console.log('++++++++++++++++Hello');
 
     const packageJson = fs.readFileSync('package.json', 'utf8');
-    console.log(packageJson, null, 2);
+    console.log(JSON.stringify(packageJson, null, 2));
   }
