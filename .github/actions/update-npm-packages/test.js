9c8ae5fed7fe4d2a2d33d5256cb1ee07d80242be
Tomasz Kasowicz <tomasz.kasowicz@gmail.com>
2025-08-28 00:53:31 +0200

    ci: actions

--- .github/actions/update-npm-packages/test.js
+++ .github/actions/update-npm-packages/test.js
@@ -1,6 +1,10 @@
 // @ts-check
 /** @param {import('@actions/github-script').AsyncFunctionArguments} AsyncFunctionArguments */
+const fs = require('fs');
 
 module.exports = async({github, context}) => {
     console.log('++++++++++++++++Hello');
+
+    const packageJson = fs.readFileSync('package.json', 'utf8');
+    console.log(packageJson, null, 2);
   }
